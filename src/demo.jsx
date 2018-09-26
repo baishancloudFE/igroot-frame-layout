9f326d8557c9a38318df3b71f635e46be6943d08
yizhen.fan <yizhen.fan@baishancloud.com>
2018-09-26 16:31:46 +0800

    feat: 0.1.15 版本发布：igroot 修复菜单模式为 horizontal 时下拉菜单无法点击的bug，随之升级

--- src/demo.jsx
+++ src/demo.jsx
@@ -18,6 +18,38 @@ const contactors = [
     qq: '614235948'
   }
 ]
+const siderMenuConfig = [
+  {
+    name: '质量概况',
+    to: '/quality',
+    key: '/quality',
+    iconType: 'solution'
+  },
+  // {
+  //   name: '调度',
+  //   to: '/dispatch',
+  //   key: '/dispatch',
+  //   iconType: 'solution'
+  // },
+  {
+    name: '配置',
+    to: '/config',
+    iconType: 'home',
+    key: '/config',
+    subs: [
+      {
+        name: '厂商配置列表',
+        to: '/config/SupplierManage',
+        key: '/SupplierManage',
+      },
+      {
+        name: '融合关系配置列表',
+        to: '/config/RelationConfigManage',
+        key: '/RelationConfigManage',
+      }
+    ]
+  }
+]
 export default () => {
   return (
 
@@ -45,22 +77,28 @@ export default () => {
       apiDomain='http://172.18.11.112:11001'
       logo="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png"
       appName="测试平台"
-      menus={JSON.parse(localStorage.getItem('MENU_INFO')).data}
-      apps={JSON.parse(localStorage.getItem('apps')).data}
-      userName={JSON.parse(localStorage.getItem('cname')).data}
+      menus={siderMenuConfig}
+      apps={{}}
+      mode="header"
+      userName={
+        'fanyizhen'
+      }
     >
       <div style={{ height: 1200, width: '100%' }}>
         <Switch>
-          {
-            (JSON.parse(localStorage.getItem('menu')) || []).map(menu => (
+          {/* {
+            siderMenuConfig.map(menu => (
               <Route exact path={menu.to} render={() => <h1>{menu.name}</h1>} />
             ))
-          }
+          } */}
+          <Route exact path='/config/SupplierManage' render={() => <h1>/config/SupplierManage</h1>} />
+          <Route exact path='/config/RelationConfigManage' render={() => <h1>/config/RelationConfigManage</h1>} />
           <Route exact path='/403' render={() => <h1>/403</h1>} />
           <Redirect exact to='/403' path='/' />
         </Switch>
       </div>
     </FrameLayout>
+
 
   )
 }
